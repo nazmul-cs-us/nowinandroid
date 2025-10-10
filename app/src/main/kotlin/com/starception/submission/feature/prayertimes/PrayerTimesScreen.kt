fb49303a95f83a51582d9f9ea33cb08746959037
nazmul-cs-us <nazmul@anyconnect.com>
2025-10-10 12:39:19 +0400

    Update PrayerTimesScreen.kt

--- app/src/main/kotlin/com/starception/submission/feature/prayertimes/PrayerTimesScreen.kt
+++ app/src/main/kotlin/com/starception/submission/feature/prayertimes/PrayerTimesScreen.kt
@@ -1113,7 +1113,7 @@ fun PrayerTimesScreen(
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         Text(
-                            text = "Pull down to refresh",
+                            text = "Pull down to refresh location",
                             style = MaterialTheme.typography.bodyMedium,
                             color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f)
                         )
